ad7d79fb0bcaf8bd2a2a3cb66e361a99a593fe00
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-13 19:16:02 +0200

    Rows AntD

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import foods from "./foods.json";
 import React, {useState} from "react";
 import FoodBox from './components/FoodBox.js';
+import { Row, Divider, Button } from 'antd';
 
 function App() {
   const [food,setFood] = useState(foods);  
@@ -12,16 +13,19 @@ function App() {
   return <div className="App">
     <h1>Food List</h1>
 
-    {foods.map((foodElem) => {
-      return (
-        <FoodBox 
-        name = {foodElem.name} 
-        calories = {foodElem.calories} 
-        image = {foodElem.image} 
-        servings = {foodElem.servings}/>
+    <Row style={{ width: '100%', justifyContent: 'center' }}>
+      {foods.map((foodElem) => {
+          return (
+            <FoodBox 
+            name = {foodElem.name} 
+            calories = {foodElem.calories} 
+            image = {foodElem.image} 
+            servings = {foodElem.servings}/>
+      
+          );
+        })}
+      </Row>
   
-      );
-    })}
   </div>;
 }
 export default App;
