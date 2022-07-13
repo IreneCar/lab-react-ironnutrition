8c49e8e630852f53aae67887c4d59b2906b885d6
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-13 19:03:14 +0200

    FoodBox component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 // src/App.js
 import './App.css';
 import foods from "./foods.json";
+import React, {useState} from "react";
+import FoodBox from './components/FoodBox.js';
 
 function App() {
-  return <div className="App"></div>;
+  const [food,setFood] = useState(foods);  
+
+
+
+  return <div className="App">
+    <h1>Food List</h1>
+
+    {foods.map((foodElem) => {
+      return (
+        <FoodBox 
+        name = {foodElem.name} 
+        calories = {foodElem.calories} 
+        image = {foodElem.image} 
+        servings = {foodElem.servings}/>
+  
+      );
+    })}
+  </div>;
 }
 export default App;
